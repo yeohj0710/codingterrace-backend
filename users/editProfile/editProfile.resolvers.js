5b5960e73624f68e1ae8f5d252f600ccf5b68e78
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-01 13:58:54 +0900

    2403011358

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,32 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => {},
+    editProfile: async (_, { id, password: newPassword, nickname }) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id,
+        },
+        data: {
+          id,
+          ...(uglyPassword && { password: uglyPassword }),
+          nickname,
+        },
+      });
+      if (!updatedUser.id) {
+        return {
+          ok: false,
+          error: "존재하지 않는 아이디입니다.",
+        };
+      }
+      return {
+        ok: true,
+      };
+    },
   },
 };
